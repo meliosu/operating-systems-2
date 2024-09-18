4be835f180a2bf330492f82d6b37bd0ecd7ef47c
meliosu <alex.atyurin@gmail.com>
2024-09-18 23:31:55 +0700

    added conditional compilation of sync primitives, add differentiation for locking read-only/read-write, added macro to explain errors

--- 2/2.3/queue.c
+++ 2/2.3/queue.c
@@ -1,9 +1,134 @@
 #define _GNU_SOURCE
 
+#include <pthread.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "queue.h"
+
+#if defined SYNC_MUTEX
+    #define SYNC_CHOSEN
+#elif defined SYNC_SPINLOCK
+    #if defined SYNC_CHOSEN
+        #define ERROR_MULTIPLE
+    #endif
+
+    #define SYNC_CHOSEN
+#elif defined SYNC_RWLOCK
+    #if defined SYNC_CHOSEN
+        #define ERROR_MULTIPLE
+    #endif
+
+    #define SYNC_CHOSEN
+#endif
+
+#if defined SYNC_MULTIPLE
+    #error "Can't combine synchronization primitives"
+#elif !defined SYNC_CHOSEN
+    #error "Must define exactly one of the following: \
+SYNC_MUTEX, SYNC_SPINLOCK, SYNC_RWLOCK"
+#endif
+
+static void node_lock(node_t *node) {
+    int err;
+
+#if defined SYNC_MUTEX
+    err = pthread_mutex_lock(&node->mutex);
+    if (err) {
+        panic("mutex_lock: %s\n", strerror(err));
+    }
+
+#elif defined SYNC_SPINLOCK
+    err = pthread_spin_lock(&node->spinlock);
+    if (err) {
+        panic("spin_lock: %s\n", strerror(err));
+    }
+
+#endif
+}
+
+void node_lock_read(node_t *node) {
+#if defined SYNC_MUTEX || defined SYNC_SPINLOCK
+    node_lock(node);
+
+#elif defined SYNC_RWLOCK
+    int err = pthread_rwlock_rdlock(&node->rwlock);
+    if (err) {
+        panic("rwlock_rdlock: %s\n", strerror(err));
+    }
+
+#endif
+}
+
+void node_lock_write(node_t *node) {
+#if defined SYNC_MUTEX || defined SYNC_SPINLOCK
+    node_lock(node);
+
+#elif defined SYNC_RWLOCK
+    int err = pthread_rwlock_wrlock(&node->rwlock);
+    if (err) {
+        panic("rwlock_wrlock: %s\n", strerror(err));
+    }
+
+#endif
+}
+
+static void node_init_lock(node_t *node) {
+    int err;
+
+#if defined SYNC_MUTEX
+    err = pthread_mutex_init(&node->mutex, NULL);
+    if (err) {
+        panic("mutex_init: %s\n", strerror(err));
+    }
+
+#elif defined SYNC_SPINLOCK
+    err = pthread_spin_init(&node->spinlock, 0);
+    if (err) {
+        panic("spin_init: %s\n", strerror(err));
+    }
+
+#elif defined SYNC_RWLOCK
+    err = pthread_rwlock_init(&node->rwlock, NULL);
+    if (err) {
+        panic("rwlock_init: %s\n", strerror(err));
+    }
+
+#endif
+}
+
+static void node_destroy_lock(node_t *node) {
+    int err;
+
+#if defined SYNC_MUTEX
+    err = pthread_mutex_destroy(&node->mutex);
+    if (err) {
+        panic("mutex_destroy: %s\n", strerror(err));
+    }
+
+#elif defined SYNC_SPINLOCK
+    err = pthread_spin_destroy(&node->spinlock);
+    if (err) {
+        panic("spin_destroy: %s\n", strerror(err));
+    }
+
+#elif defined SYNC_RWLOCK
+    err = pthread_rwlock_destroy(&node->rwlock);
+    if (err) {
+        panic("rwlock_destroy: %s\n", strerror(err));
+    }
+
+#endif
+}
+
+static void node_fill_data(node_t *node) {
+    for (int i = 0; i < 100; i++) {
+        node->value[i] = 'a' + rand() % ('z' - 'a');
+    }
+
+    node->value[rand() % 100] = 0;
+    node->next = NULL;
+}
 
 node_t *node_random() {
     node_t *node = malloc(sizeof(node_t));
@@ -12,19 +137,8 @@ node_t *node_random() {
         panic("OOM");
     }
 
-    for (int i = 0; i < 100; i++) {
-        node->value[i] = 'a' + rand() % ('z' - 'a');
-    }
-
-    node->value[rand() % 100] = 0;
-    node->next = NULL;
-
-    int err;
-
-    err = pthread_mutex_init(&node->mutex, NULL);
-    if (err) {
-        panic("pthread_mutex_init: %s\n", strerror(err));
-    }
+    node_fill_data(node);
+    node_init_lock(node);
 
     return node;
 }
@@ -32,10 +146,8 @@ node_t *node_random() {
 void queue_init(queue_t *queue, int size) {
     queue->head = NULL;
 
-    node_t *curr;
-
     for (int i = 0; i < size; i++) {
-        curr = node_random();
+        node_t *curr = node_random();
         curr->next = queue->head;
         queue->head = curr;
     }
@@ -48,14 +160,8 @@ void queue_destroy(queue_t *queue) {
 
     while (curr) {
         node_t *next = curr->next;
-
-        err = pthread_mutex_destroy(&curr->mutex);
-        if (err) {
-            printf("pthread_mutex_destroy: %s\n", strerror(err));
-        }
-
+        node_destroy_lock(curr);
         free(curr);
-
         curr = next;
     }
 }
--- 2/2.3/queue.h
+++ 2/2.3/queue.h
@@ -11,7 +11,17 @@ typedef struct node_t {
     char value[100];
     struct node_t *next;
 
+#if defined SYNC_MUTEX
     pthread_mutex_t mutex;
+
+#elif defined SYNC_RWLOCK
+    pthread_rwlock_t rwlock;
+
+#elif defined SYNC_SPINLOCK
+    pthread_spinlock_t spinlock;
+
+#endif
+
 } node_t;
 
 typedef struct queue_t {
@@ -19,6 +29,9 @@ typedef struct queue_t {
 } queue_t;
 
 node_t *node_random();
+
+void node_lock_read(node_t *node);
+void node_lock_write(node_t *node);
 
 void queue_init(queue_t *queue, int size);
 void queue_destroy(queue_t *queue);
