aa470fe3c9558fc6b3ba16fb915b9f70633dc705
meliosu <alex.atyurin@gmail.com>
2024-09-18 22:44:17 +0700

    added swapping neighbour nodes, counter tracking

--- 2/2.3/main.c
+++ 2/2.3/main.c
@@ -3,8 +3,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "queue.h"
+
+#define SWAP_PROBABILITY 10
+
+int counter_increasing;
+int counter_decreasing;
+int counter_equal;
+int counter_swapped;
 
 int increasing(char *a, char *b) {
     return strlen(a) < strlen(b);
@@ -43,10 +51,9 @@ void traverse_compare(
 
 void *thread_increasing(void *arg) {
     queue_t *queue = arg;
-    int counter;
 
     while (1) {
-        traverse_compare(queue, &counter, increasing);
+        traverse_compare(queue, &counter_increasing, increasing);
     }
 
     return NULL;
@@ -54,10 +61,9 @@ void *thread_increasing(void *arg) {
 
 void *thread_decreasing(void *arg) {
     queue_t *queue = arg;
-    int counter;
 
     while (1) {
-        traverse_compare(queue, &counter, decreasing);
+        traverse_compare(queue, &counter_decreasing, decreasing);
     }
 
     return NULL;
@@ -65,21 +71,74 @@ void *thread_decreasing(void *arg) {
 
 void *thread_equal(void *arg) {
     queue_t *queue = arg;
-    int counter;
 
     while (1) {
-        traverse_compare(queue, &counter, equal);
+        traverse_compare(queue, &counter_equal, equal);
     }
 
     return NULL;
 }
 
+void swap_nodes(node_t *prev, node_t *a, node_t *b) {
+    prev->next = b;
+    a->next = b->next;
+    b->next = a;
+}
+
+void traverse_permute(queue_t *queue, int *counter) {
+    node_t *first = queue->head;
+
+    pthread_mutex_lock(&first->mutex);
+    node_t *second = first->next;
+
+    pthread_mutex_lock(&second->mutex);
+    node_t *third = second->next;
+
+    while (third) {
+        pthread_mutex_lock(&third->mutex);
+
+        if (rand() < RAND_MAX / SWAP_PROBABILITY) {
+            swap_nodes(first, second, third);
+        }
+
+        node_t *fourth = third->next;
+
+        pthread_mutex_unlock(&first->mutex);
+
+        first = second;
+        second = third;
+        third = fourth;
+    }
+
+    pthread_mutex_unlock(&first->mutex);
+    pthread_mutex_unlock(&second->mutex);
+
+    __sync_fetch_and_add(counter, 1);
+}
+
 void *thread_permutating(void *arg) {
     queue_t *queue = arg;
 
-    // TODO ...
+    while (1) {
+        traverse_permute(queue, &counter_swapped);
+    }
 
     return NULL;
+}
+
+void print_counters() {
+    printf(
+        "%10s %10s %10s %10s %10s\n", "incr.", "decr.", "eq.", "total", "swap"
+    );
+
+    printf(
+        "%10d %10d %10d %10d %10d\n",
+        counter_increasing,
+        counter_decreasing,
+        counter_equal,
+        counter_increasing + counter_decreasing + counter_swapped,
+        counter_swapped
+    );
 }
 
 int main() {
@@ -103,6 +162,11 @@ int main() {
         if (err) {
             panic("main: pthread_create: %s\n", strerror(err));
         }
+    }
+
+    while (1) {
+        print_counters();
+        sleep(1);
     }
 
     for (int i = 0; i < 6; i++) {
