4a689f37d8da9900a17b72b0d5d714f1e8cd5a4c
meliosu <alex.atyurin@gmail.com>
2024-09-19 22:47:43 +0700

    moved stack creation from header

--- 1/uthreads.c
+++ 1/uthreads.c
@@ -5,10 +5,37 @@
 #include <sys/mman.h>
 #include <unistd.h>
 
-#include "stack.h"
 #include "uthreads.h"
 
 #define STACK_SIZE (512 * 1024)
+#define PAGE_SIZE 4096
+
+void *create_stack(int size) {
+    int err;
+
+    void *stack = mmap(
+        NULL, size, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_STACK, -1, 0
+    );
+
+    if (stack == MAP_FAILED) {
+        return NULL;
+    }
+
+    err = mprotect(
+        (char *)stack + PAGE_SIZE, size - PAGE_SIZE, PROT_READ | PROT_WRITE
+    );
+
+    if (err) {
+        munmap(stack, size);
+        return NULL;
+    }
+
+    return stack;
+}
+
+void destroy_stack(void *stack, int size) {
+    munmap(stack, size);
+}
 
 static struct sched_ctx sched_ctx = {
     .init = 0,
