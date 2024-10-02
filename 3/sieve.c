f577e78518220f2d5987d6e2590b59b23a5f9d8d
Alexander <alex.atyurin@gmail.com>
2024-10-03 01:34:45 +0700

    SIEVE cache

---
+++ 3/sieve.c
@@ -0,0 +1,104 @@
+#include <pthread.h>
+#include <string.h>
+
+#include "hashmap.h"
+#include "sieve.h"
+
+void sieve_cache_init(struct cache *cache, int cap) {
+    memset(cache, 0, sizeof(*cache));
+    cache->cap = cap;
+    hashmap_init(&cache->map, cap);
+    pthread_rwlock_init(&cache->lock, NULL);
+}
+
+void sieve_cache_destroy(struct cache *cache) {
+    pthread_rwlock_destroy(&cache->lock);
+    hashmap_destroy(&cache->map);
+}
+
+void sieve_cache_lookup(struct cache *cache, slice_t request, void **response) {
+    pthread_rwlock_rdlock(&cache->lock);
+
+    struct cache_entry *cache_entry;
+    hashmap_get(&cache->map, request, &cache_entry);
+
+    if (cache_entry) {
+        *response = cache_entry->response;
+        bool visited = 1;
+        __atomic_store(&cache_entry->visited, &visited, __ATOMIC_RELAXED);
+    }
+
+    pthread_rwlock_unlock(&cache->lock);
+}
+
+static void sieve_cache_evict(struct cache *cache) {
+    struct cache_entry *curr = cache->hand;
+
+    while (curr) {
+        if (!curr->visited) {
+            if (curr == cache->tail) {
+                curr->next->prev = NULL;
+                cache->tail = curr->next;
+                cache->hand = curr->next;
+            } else if (curr == cache->head) {
+                curr->prev->next = NULL;
+                cache->head = curr->prev;
+                cache->hand = cache->tail;
+            } else {
+                curr->next->prev = curr->prev;
+                curr->prev->next = curr->next;
+                cache->hand = curr->next;
+            }
+
+            break;
+        }
+
+        curr->visited = 0;
+
+        if (!curr->next) {
+            curr = cache->tail;
+        } else {
+            curr = curr->next;
+        }
+    }
+
+    hashmap_remove(&cache->map, curr->request, NULL);
+    free(curr);
+
+    // NOTE: maybe cleanup response stuff also?
+}
+
+static void sieve_cache_insert_nonfull(
+    struct cache *cache, slice_t request, struct cache_entry *entry
+) {
+    if (!cache->head) {
+        entry->prev = entry->next = NULL;
+        cache->head = cache->tail = cache->hand = entry;
+    } else {
+        cache->head->next = entry;
+        entry->prev = cache->head;
+        cache->head = entry;
+    }
+
+    hashmap_insert(&cache->map, request, entry);
+
+    cache->len += 1;
+}
+
+void sieve_cache_insert(struct cache *cache, slice_t request, void *response) {
+    struct cache_entry *entry = malloc(sizeof(*entry));
+
+    entry->next = entry->prev = NULL;
+    entry->request = request;
+    entry->response = response;
+
+    pthread_rwlock_wrlock(&cache->lock);
+
+    if (cache->len == cache->cap) {
+        sieve_cache_evict(cache);
+    }
+
+    sieve_cache_insert_nonfull(cache, request, entry);
+
+    pthread_rwlock_unlock(&cache->lock);
+}
---
+++ 3/sieve.h
@@ -0,0 +1,37 @@
+#ifndef PROXY_SIEVE_H
+#define PROXY_SIEVE_H
+
+#include <stdbool.h>
+
+#include <pthread.h>
+
+#include "hashmap.h"
+#include "slice.h"
+
+struct cache_entry {
+    slice_t request;
+    void *response;
+
+    bool visited;
+    struct cache_entry *prev;
+    struct cache_entry *next;
+};
+
+struct cache {
+    int len;
+    int cap;
+    struct hashmap map;
+
+    struct cache_entry *head;
+    struct cache_entry *tail;
+    struct cache_entry *hand;
+
+    pthread_rwlock_t lock;
+};
+
+void sieve_cache_init(struct cache *cache, int cap);
+void sieve_cache_destroy(struct cache *cache);
+void sieve_cache_lookup(struct cache *cache, slice_t request, void **response);
+void sieve_cache_insert(struct cache *cache, slice_t request, void *response);
+
+#endif /* PROXY_SIEVE_H */
