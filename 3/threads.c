0c077be74c30d8a8bf38213d331a092e5047ac6e
Alexander <alex.atyurin@gmail.com>
2024-10-03 19:50:20 +0700

    added files for client/server handling threads

---
+++ 3/threads.c
@@ -0,0 +1,20 @@
+#include <errno.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <netdb.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include "threads.h"
+
+void *thread_clientside(void *arg) {
+    return NULL;
+}
+
+void *thread_serverside(void *arg) {
+    return NULL;
+}
