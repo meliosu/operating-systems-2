2e5f9da195981f9f8f3840c78ea6c476d48dcb09
Alexander <alex.atyurin@gmail.com>
2024-10-04 23:56:17 +0700

    added dependency on libllhttp

--- 3/http.c
+++ 3/http.c
@@ -1,68 +1,99 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "http.h"
 
-void http_request_init(struct http_request *request) {
-    request->version.major = 0;
-    request->version.minor = 0;
-    request->method = NULL;
-    request->url = NULL;
+static int on_method_complete(llhttp_t *parser) {
+    struct http_request *request = parser->data;
+    request->method = llhttp_get_method(parser);
+    return 0;
 }
 
-void http_request_destroy(struct http_request *request) {
-    free(request->method);
-    free(request->url);
+static int on_url(llhttp_t *parser, const char *url, unsigned long len) {
+    struct http_request *request = parser->data;
+    request->url = strndup(url, len);
+    return 0;
+}
+
+static int on_version_complete_request(llhttp_t *parser) {
+    struct http_request *request = parser->data;
+    request->version.major = llhttp_get_http_major(parser);
+    request->version.minor = llhttp_get_http_minor(parser);
+    return 0;
+}
+
+static int on_version_complete_response(llhttp_t *parser) {
+    struct http_response *response = parser->data;
+    response->version.major = llhttp_get_http_major(parser);
+    response->version.minor = llhttp_get_http_minor(parser);
+    return 0;
+}
+
+static int on_request_complete(llhttp_t *parser) {
+    struct http_request *request = parser->data;
+    request->finished = 1;
+    return 0;
+}
+
+static int on_response_complete(llhttp_t *parser) {
+    struct http_response *response = parser->data;
+    response->finished = 1;
+    return 0;
+}
+
+static int on_status_complete(llhttp_t *parser) {
+    struct http_response *response = parser->data;
+    response->status = llhttp_get_status_code(parser);
+    return 0;
+}
+
+void http_request_init(struct http_request *request) {
+    memset(request, 0, sizeof(struct http_request));
+
+    llhttp_settings_init(&request->settings);
+    request->settings.on_message_complete = on_request_complete;
+    request->settings.on_version_complete = on_version_complete_request;
+    request->settings.on_url = on_url;
+    request->settings.on_method_complete = on_method_complete;
+
+    llhttp_init(&request->parser, HTTP_REQUEST, &request->settings);
+    request->parser.data = request;
 }
 
 void http_response_init(struct http_response *response) {
-    response->version.major = 0;
-    response->version.minor = 0;
-    response->status = 0;
-    response->phrase = NULL;
+    memset(response, 0, sizeof(struct http_response));
+
+    llhttp_settings_init(&response->settings);
+    response->settings.on_message_complete = on_response_complete;
+    response->settings.on_version_complete = on_version_complete_response;
+    response->settings.on_status_complete = on_status_complete;
+
+    llhttp_init(&response->parser, HTTP_RESPONSE, &response->settings);
+    response->parser.data = response;
 }
 
-void http_response_destroy(struct http_response *response) {
-    free(response->phrase);
-}
-
-int http_request_parse(struct http_request *request, char *buffer, int len) {
-    // TODO: handle incomplete input
-
-    int matched = sscanf(
-        buffer,
-        "%ms %ms HTTP/%d.%d\r\n",
-        &request->method,
-        &request->url,
-        &request->version.major,
-        &request->version.minor
-    );
-
-    if (matched < 4) {
-        // TODO: free memory allocated for strings
-
+int http_request_parse(struct http_request *request, char *buf, int len) {
+    llhttp_errno_t err = llhttp_execute(&request->parser, buf, len);
+    if (err != HPE_OK) {
         return -1;
+    }
+
+    if (!request->finished) {
+        return -2;
     }
 
     return 0;
 }
 
-int http_response_parse(struct http_response *response, char *buffer, int len) {
-    // TODO: handle incomplete input
-
-    int matched = sscanf(
-        buffer,
-        "HTTP/%d.%d %d %m[^\r\n]\r\n",
-        &response->version.major,
-        &response->version.minor,
-        &response->status,
-        &response->phrase
-    );
-
-    if (matched < 4) {
-        // TODO: free memory allocated for strings
-
+int http_response_parse(struct http_response *response, char *buf, int len) {
+    llhttp_errno_t err = llhttp_execute(&response->parser, buf, len);
+    if (err != HPE_OK) {
         return -1;
+    }
+
+    if (!response->finished) {
+        return -2;
     }
 
     return 0;
--- 3/http.h
+++ 3/http.h
@@ -1,31 +1,37 @@
 #ifndef PROXY_HTTP_H
 #define PROXY_HTTP_H
 
+#include <llhttp.h>
+
 struct http_request {
-    char *method;
-    char *url;
     struct {
         int major;
         int minor;
     } version;
+    int method;
+    char *url;
+
+    int finished;
+    llhttp_t parser;
+    llhttp_settings_t settings;
 };
 
 struct http_response {
-    int status;
-    char *phrase;
     struct {
         int major;
         int minor;
     } version;
+    int status;
+
+    int finished;
+    llhttp_t parser;
+    llhttp_settings_t settings;
 };
 
 void http_request_init(struct http_request *request);
-void http_request_destroy(struct http_request *request);
-
 void http_response_init(struct http_response *response);
-void http_response_destroy(struct http_response *response);
 
-int http_request_parse(struct http_request *request, char *buffer, int len);
-int http_response_parse(struct http_response *response, char *buffer, int len);
+int http_request_parse(struct http_request *request, char *buf, int len);
+int http_response_parse(struct http_response *response, char *buf, int len);
 
 #endif /* PROXY_HTTP_H */
