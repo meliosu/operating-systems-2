42b5ba360987c48d16abd7a08ae3b932310c40e5
Alexander <alex.atyurin@gmail.com>
2024-10-04 18:38:28 +0700

    added SIEVE cache

---
+++ 3/sieve.h
@@ -0,0 +1,34 @@
+#ifndef PROXY_SIEVE_H
+#define PROXY_SIEVE_H
+
+#include <pthread.h>
+#include <stdatomic.h>
+
+#include "hashmap.h"
+
+struct cache_entry {
+    char *key;
+    void *value;
+
+    atomic_bool visited;
+    struct cache_entry *prev;
+    struct cache_entry *next;
+};
+
+struct cache {
+    int len;
+    int cap;
+    struct hashmap map;
+    struct cache_entry *head;
+    struct cache_entry *tail;
+    struct cache_entry *hand;
+    pthread_rwlock_t lock;
+};
+
+void sieve_cache_init(struct cache *cache, int cap);
+void sieve_cache_destroy(struct cache *cache);
+
+void sieve_cache_insert(struct cache *cache, char *key, void *value);
+void sieve_cache_lookup(struct cache *cache, char *key, void **value);
+
+#endif /* PROXY_SIEVE_H */
