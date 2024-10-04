d518679a5cb5bf4e94a00877702d7c493409b89d
Alexander <alex.atyurin@gmail.com>
2024-10-04 19:20:03 +0700

    added init/destroy functions for request and response

--- 3/http.c
+++ 3/http.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "http.h"
+
+void http_request_init(struct http_request *request) {
+    request->version.major = 0;
+    request->version.minor = 0;
+    request->method = NULL;
+    request->url = NULL;
+}
+
+void http_request_destroy(struct http_request *request) {
+    free(request->method);
+    free(request->url);
+}
+
+void http_response_init(struct http_response *response) {
+    response->version.major = 0;
+    response->version.minor = 0;
+    response->status = 0;
+    response->phrase = NULL;
+}
+
+void http_response_destroy(struct http_response *response) {
+    free(response->phrase);
+}
 
 int http_request_parse(struct http_request *request, char *buffer, int len) {
     // TODO: handle incomplete input
@@ -15,6 +39,8 @@ int http_request_parse(struct http_request *request, char *buffer, int len) {
     );
 
     if (matched < 4) {
+        // TODO: free memory allocated for strings
+
         return -1;
     }
 
@@ -34,6 +60,8 @@ int http_response_parse(struct http_response *response, char *buffer, int len) {
     );
 
     if (matched < 4) {
+        // TODO: free memory allocated for strings
+
         return -1;
     }
 
--- 3/http.h
+++ 3/http.h
@@ -19,6 +19,12 @@ struct http_response {
     } version;
 };
 
+void http_request_init(struct http_request *request);
+void http_request_destroy(struct http_request *request);
+
+void http_response_init(struct http_response *response);
+void http_response_destroy(struct http_response *response);
+
 int http_request_parse(struct http_request *request, char *buffer, int len);
 int http_response_parse(struct http_response *response, char *buffer, int len);
 
